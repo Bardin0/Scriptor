bf78ae77f20f4a3bd7db095d21e9e7396ca32189
Michael Giovannini <bardin@michaels-mbp-6.home>
2024-04-26 14:48:06 -0400

    added reading from a file

--- scriptor.c
+++ scriptor.c
@@ -1,11 +1,16 @@
 /*** includes ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -28,10 +33,17 @@ enum editorKey {
 
 /*** data ***/
 
+typedef struct erow{
+    int size;
+    char *chars;
+} erow;
+
 struct editorConfig {
     int cx, cy;
     int screenrows;
     int screencols;
+    int numrows;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -152,6 +164,37 @@ int getWindowSize (int *rows, int *cols){
     }
 }
 
+/*** row operations ***/
+
+void editorAppendRow(char *s, size_t len) {
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
+/*** file i/o ***/
+void editorOpen(char *filename){
+    FILE *fp = fopen(filename, "r");
+    if (!fp) die("fopen");
+
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    while ((linelen = getline(&line, &linecap, fp)) != -1) {
+        while (linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
+            linelen--;
+        editorAppendRow(line, linelen);
+
+    }
+    free(line);
+    fclose(fp);
+}
+
 /*** append buffer***/
 
 struct abuf{
@@ -243,7 +286,8 @@ void editorProcessKeypress() {
 void editorDrawRows(struct abuf *ab){
     int y;
     for(int y = 0; y < E.screenrows; y++){
-        if (y == E.screenrows/3){
+        if(y >= E.numrows){
+        if (E.numrows && y == E.screenrows/3){
             char welcome[80];
             int welcomelen = snprintf(welcome, sizeof(welcome), "Scriptor editor -- version %s", SCRIPTOR_VERSION);
             if(welcomelen > E.screencols) welcomelen = E.screencols;
@@ -256,6 +300,11 @@ void editorDrawRows(struct abuf *ab){
             abAppend(ab, welcome, welcomelen);
         } else {
             abAppend(ab, "~", 1);
+        }
+        }else{
+            int len = E.row[y].size;
+            if (len > E.screencols) len = E.screencols;
+            abAppend(ab, E.row[y].chars, len);
         }
 
         abAppend(ab, "\x1b[K", 3);
@@ -288,13 +337,18 @@ void editorRefreshScreen(){
 void initEditor(){
     E.cx = 0;
     E.cy = 0;
+    E.numrows = 0;
+    E.row = NULL;
 
     if(getWindowSize(&E.screenrows, &E.screencols) == -1) die ("getWindowSize");
 }
 
-int main(){
+int main(int argc, char *argv[]){
     enableRawMode();
     initEditor();
+    if(argc >= 2){
+        editorOpen(argv[1]);
+    }
 
     while (1){
         editorRefreshScreen();
