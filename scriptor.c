ff231b978d64390db8d23ccfac1cf559c6226ee3
Michael Giovannini <bardin@Michaels-MacBook-Pro-6.local>
2024-04-28 20:01:12 -0400

    Added status bar

--- scriptor.c
+++ scriptor.c
@@ -7,7 +7,9 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
+#include <stdarg.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 /*** defines ***/
@@ -49,6 +51,9 @@ struct editorConfig {
     int screencols; //Stores number of columns in terminal
     int numrows;    //Stores number of rows in file
     erow *row;
+    char *filename;
+    char statusmsg[80];
+    time_t statusmsg_time;
     struct termios orig_termios;
 };
 
@@ -89,23 +94,18 @@ void enableRawMode(){
 int editorReadKey() {
     int nread;
     char c;
-
-    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) { //Reads one byte from STDIN_FILENO and stores it in c
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
-
-    if (c == '\x1b'){ //If c is an escape character
+    if (c == '\x1b') {
         char seq[3];
-
-        if(read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b'; 
-        if(read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b'; 
-
-        //Checks if special characters were input and returns the appropriate key
-        if(seq[0] == '['){ 
-            if (seq[1] >= '0' && seq[1] <= '9'){ 
-                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b'; 
-                if (seq[2] == '~'){
-                    switch(seq[1]){
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+        if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
                         case '1': return HOME_KEY;
                         case '3': return DEL_KEY;
                         case '4': return END_KEY;
@@ -114,9 +114,9 @@ int editorReadKey() {
                         case '7': return HOME_KEY;
                         case '8': return END_KEY;
                     }
-                }   
-            }else { //If no digit follows the escape key then it is an arrow key and returns correct one
-                switch (seq[1]){
+                }
+            } else {
+                switch (seq[1]) {
                     case 'A': return ARROW_UP;
                     case 'B': return ARROW_DOWN;
                     case 'C': return ARROW_RIGHT;
@@ -125,17 +125,15 @@ int editorReadKey() {
                     case 'F': return END_KEY;
                 }
             }
-            //If the first character is 'O' then it is an arrow key that maps to the home or end key
-        }else if (seq[0] == 'O'){
-            switch (seq[1]){
+        } else if (seq[0] == 'O') {
+            switch (seq[1]) {
                 case 'H': return HOME_KEY;
                 case 'F': return END_KEY;
             }
         }
-
         return '\x1b';
-    } else{
-        return c;
+    } else {
+    return c;
     }
 }
 
@@ -226,7 +224,10 @@ void editorAppendRow(char *s, size_t len) {
 
 /*** file i/o ***/
 void editorOpen(char *filename){
-    FILE *fp = fopen(filename, "r");
+    free(E.filename);
+    E.filename = strdup(filename);  //Dupes filename
+
+    FILE *fp = fopen(filename, "r");    //Opens file
     if (!fp) die("fopen");
 
     char *line = NULL;
@@ -321,15 +322,23 @@ void editorProcessKeypress() {
         break;
     
     case END_KEY:
-        E.cx = E.screencols -1;
+        if (E.cy < E.numrows)
+            E.cx = E.row[E.cy].size;
         break;
 
     case PAGE_UP:
     case PAGE_DOWN:
         {
+            if (c == PAGE_UP){
+                E.cy = E.rowoff;
+            }else if (c == PAGE_DOWN){
+                E.cy = E.rowoff + E.screenrows - 1;
+                if(E.cy > E.numrows) E.cy = E.numrows;
+            }
+
             int times = E.screenrows;
             while (times--)
-                editorMoveCursor(c == PAGE_UP ? ARROW_UP: ARROW_DOWN);
+                editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
         }
         break;
     
@@ -392,10 +401,38 @@ void editorDrawRows(struct abuf *ab){
         }
 
         abAppend(ab, "\x1b[K", 3);
-        if(y < E.screenrows - 1){
-            abAppend(ab, "\r\n", 2);
+        abAppend(ab, "\r\n", 2);
+    }
+}
+
+void editorDrawStatusBar(struct abuf *ab) {
+    abAppend(ab, "\x1b[7m", 4);
+    char status[80], rstatus[80];
+
+    int len = snprintf(status, sizeof(status), "%.20s - %d lines",E.filename ? E.filename : "[No Name]", E.numrows);
+    int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",E.cy + 1, E.numrows);
+
+    if (len > E.screencols) len = E.screencols;
+    abAppend(ab, status, len);
+    while (len < E.screencols) {
+        if (E.screencols - len == rlen) {
+            abAppend(ab, rstatus, rlen);
+            break;
+        } else {
+            abAppend(ab, " ", 1);
+            len++;
         }
     }
+  abAppend(ab, "\x1b[m", 3);
+  abAppend(ab, "\r\n", 2);
+}
+
+void editorDrawMessageBar(struct abuf *ab) {
+    abAppend(ab, "\x1b[K", 3);
+    int msglen = strlen(E.statusmsg);
+    if (msglen > E.screencols) msglen = E.screencols;
+    if (msglen && time(NULL) - E.statusmsg_time < 5)
+        abAppend(ab, E.statusmsg, msglen);
 }
 
 void editorRefreshScreen(){
@@ -407,6 +444,8 @@ void editorRefreshScreen(){
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
+    editorDrawStatusBar(&ab);
+    editorDrawMessageBar(&ab);
 
     char buf[32];
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,(E.rx - E.coloff) + 1);
@@ -416,6 +455,14 @@ void editorRefreshScreen(){
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
+}
+
+void editorSetStatusMessage(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+  va_end(ap);
+  E.statusmsg_time = time(NULL);
 }
 
 /*** init ***/
@@ -428,8 +475,12 @@ void initEditor(){
     E.rx = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.filename = NULL;
+    E.statusmsg[0] = '\0';
+    E.statusmsg_time = 0;
 
     if(getWindowSize(&E.screenrows, &E.screencols) == -1) die ("getWindowSize");    //Gets window size
+    E.screenrows -= 2;
 }
 
 int main(int argc, char *argv[]){
@@ -438,6 +489,8 @@ int main(int argc, char *argv[]){
     if(argc >= 2){
         editorOpen(argv[1]);
     }
+
+    editorSetStatusMessage("HELP: Ctrl-Q = quit");
 
     while (1){
         editorRefreshScreen();
