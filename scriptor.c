67283ac6e633d63b9ea462d023798dce9ea37011
Michael Giovannini <154377009+Bardin0@users.noreply.github.com>
2024-05-04 12:10:08 -0400

    Added keyword highllighting for c programs

--- scriptor.c
+++ scriptor.c
@@ -43,6 +43,9 @@ enum editorKey {
 
 enum editorHighlight {
   HL_NORMAL = 0,
+  HL_COMMENT,
+  HL_KEYWORD1,
+  HL_KEYWORD2,
   HL_STRING,
   HL_NUMBER,
   HL_MATCH
@@ -53,6 +56,8 @@ enum editorHighlight {
 struct editorSyntax {
   char *filetype;   //Name of the filetype to be displayed in status bar
   char **filematch; //Array of strings to compare against the filetype
+  char **keywords;  //Array of strings to be highlighted
+  char *singleline_comment_start;
   int flags;    //Contains a bitfield to track what to be highlighted based on filetype
 };
 
@@ -90,14 +95,21 @@ struct editorConfig E;
 
 /*** filetypes ***/
 
-char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL }; //C/C++ file extensions
-
+char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
+char *C_HL_keywords[] = {
+  "switch", "if", "while", "for", "break", "continue", "return", "else",
+  "struct", "union", "typedef", "static", "enum", "class", "case",
+  "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
+  "void|", NULL
+};
 struct editorSyntax HLDB[] = {
-  {
-    "c",
-    C_HL_extensions,
-    HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
-  },
+    {
+        "c",
+        C_HL_extensions,
+        C_HL_keywords,
+        "//",
+        HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
+    },
 };
 
 /*** prototypes ***/
@@ -237,14 +249,22 @@ void editorUpdateSyntax(erow *row) {
 
     if (E.syntax == NULL) return;
 
+    char **keywords = E.syntax->keywords;
+    char *scs = E.syntax->singleline_comment_start;
+    int scs_len = scs ? strlen(scs) : 0;
     int prev_sep = 1;
     int in_string = 0;
-
     int i = 0;
+
     while (i < row->rsize) {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
-
+        if (scs_len && !in_string) {
+            if (!strncmp(&row->render[i], scs, scs_len)) {
+                memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+                break;
+            }
+        }
         if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
             if (in_string) {
                 row->hl[i] = HL_STRING;
@@ -259,18 +279,34 @@ void editorUpdateSyntax(erow *row) {
                 continue;
             } else {
                 if (c == '"' || c == '\'') {
-                in_string = c;
-                row->hl[i] = HL_STRING;
-                i++;
-                continue;
+                    in_string = c;
+                    row->hl[i] = HL_STRING;
+                    i++;
+                    continue;
                 }
             }
         }
-
         if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
             if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)) {
                 row->hl[i] = HL_NUMBER;
                 i++;
+                prev_sep = 0;
+                continue;
+            }
+        }
+        if (prev_sep) {
+            int j;
+            for (j = 0; keywords[j]; j++) {
+                int klen = strlen(keywords[j]);
+                int kw2 = keywords[j][klen - 1] == '|';
+                if (kw2) klen--;
+                if (!strncmp(&row->render[i], keywords[j], klen) && is_separator(row->render[i + klen])) {
+                    memset(&row->hl[i], kw2 ? HL_KEYWORD2 : HL_KEYWORD1, klen);
+                    i += klen;
+                    break;
+                }
+            }
+            if (keywords[j] != NULL) {
                 prev_sep = 0;
                 continue;
             }
@@ -282,6 +318,9 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
   switch (hl) {
+    case HL_COMMENT: return 36;
+    case HL_KEYWORD1: return 33;
+    case HL_KEYWORD2: return 32;
     case HL_STRING: return 35;
     case HL_NUMBER: return 31;
     case HL_MATCH: return 34;
